72daf176b4765bc3cb41c0c4c4c20f22e7b38246
tzvetkov75 <tzvetkov@bg-soft.de>
2014-01-30 19:46:22 +0100

    layout port draft

---
+++ README.md
@@ -0,0 +1,4 @@
+ This is Android APP for recording fitness exerciese 
+============================================
+
+	This APP records and should upload to g-drive for your statistic 
--- gen/com/example/fitnessnotes/R.java
+++ gen/com/example/fitnessnotes/R.java
@@ -25,22 +25,22 @@ public final class R {
     }
     public static final class id {
         public static final int action_settings=0x7f080010;
-        public static final int button1=0x7f080009;
+        public static final int button0=0x7f08000c;
+        public static final int button1=0x7f08000b;
+        public static final int button10=0x7f080008;
         public static final int button2=0x7f08000a;
-        public static final int button3=0x7f08000b;
-        public static final int button4=0x7f08000c;
-        public static final int button5=0x7f08000d;
-        public static final int button6=0x7f08000f;
-        public static final int button7=0x7f08000e;
-        public static final int editText1=0x7f080002;
-        public static final int editText2=0x7f080004;
-        public static final int editText3=0x7f080006;
+        public static final int button5=0x7f080009;
+        public static final int buttonNewTraining=0x7f08000d;
+        public static final int buttonStopTraining=0x7f08000e;
+        public static final int buttonStore=0x7f08000f;
+        public static final int editExerciese=0x7f080002;
+        public static final int editTextWeight=0x7f080004;
+        public static final int exerciese=0x7f080001;
         public static final int linearLayout1=0x7f080000;
-        public static final int textView1=0x7f080001;
-        public static final int textView2=0x7f080003;
+        public static final int listViewHistory=0x7f080007;
         public static final int textView3=0x7f080005;
-        public static final int textView4=0x7f080007;
-        public static final int textView5=0x7f080008;
+        public static final int textView4=0x7f080006;
+        public static final int viewTextRepetition=0x7f080003;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -51,7 +51,20 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int button0=0x7f050007;
+        public static final int button1=0x7f050006;
+        public static final int button10=0x7f050004;
+        public static final int button2=0x7f050003;
+        public static final int button5=0x7f050005;
+        public static final int buttonNewTraining=0x7f050008;
+        public static final int buttonStopTraining=0x7f050009;
+        public static final int buttonStore=0x7f05000a;
+        public static final int general=0x7f05000f;
         public static final int hello_world=0x7f050002;
+        public static final int textViewExercise=0x7f05000e;
+        public static final int textViewHistory=0x7f05000b;
+        public static final int textViewRepetitions=0x7f05000d;
+        public static final int textViewWeight=0x7f05000c;
     }
     public static final class style {
         /** 
