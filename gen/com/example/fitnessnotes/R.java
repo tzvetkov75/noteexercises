d0992cac8fcfcec665e3547d4cfd2d79ed013b5e
tzvetkov75 <tzvetkov@bg-soft.de>
2014-01-31 14:19:17 +0100

    main activity draft

--- gen/com/example/fitnessnotes/R.java
+++ gen/com/example/fitnessnotes/R.java
@@ -24,22 +24,23 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080010;
-        public static final int button0=0x7f08000c;
-        public static final int button1=0x7f08000b;
-        public static final int button10=0x7f080008;
-        public static final int button2=0x7f08000a;
-        public static final int button5=0x7f080009;
-        public static final int buttonNewTraining=0x7f08000d;
-        public static final int buttonStopTraining=0x7f08000e;
-        public static final int buttonStore=0x7f08000f;
+        public static final int action_settings=0x7f080011;
+        public static final int button0=0x7f08000d;
+        public static final int button1=0x7f08000c;
+        public static final int button10=0x7f080009;
+        public static final int button2=0x7f08000b;
+        public static final int button5=0x7f08000a;
+        public static final int buttonNewTraining=0x7f08000e;
+        public static final int buttonStopTraining=0x7f08000f;
+        public static final int buttonStore=0x7f080010;
         public static final int editExerciese=0x7f080002;
-        public static final int editTextWeight=0x7f080004;
+        public static final int editTextRepetitions=0x7f080004;
         public static final int exerciese=0x7f080001;
+        public static final int exitTextWeight=0x7f080006;
         public static final int linearLayout1=0x7f080000;
-        public static final int listViewHistory=0x7f080007;
+        public static final int listViewHistory=0x7f080008;
         public static final int textView3=0x7f080005;
-        public static final int textView4=0x7f080006;
+        public static final int textView4=0x7f080007;
         public static final int viewTextRepetition=0x7f080003;
     }
     public static final class layout {
--- src/com/example/fitnessnotes/MainActivity.java
+++ src/com/example/fitnessnotes/MainActivity.java
@@ -2,22 +2,99 @@ package com.example.fitnessnotes;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.graphics.Color;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.graphics.PorterDuff;
+import android.view.*;
+
 
 public class MainActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	private EditText edRep, edWei, ed;
+	private int tempInt = 0;
+	 
 
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		edRep=(EditText)findViewById(R.id.editTextRepetitions);
+		edWei=(EditText)findViewById(R.id.exitTextWeight);
+		
+	    edRep.setOnTouchListener(new View.OnTouchListener()
+	        {
+	            public boolean onTouch(View arg0, MotionEvent arg1)
+	            {
+	            	ed = (EditText) arg0;
+	                return false;
+	            }
+	        });
+	    
+	    edWei.setOnTouchListener(new View.OnTouchListener()
+        {
+            public boolean onTouch(View arg0, MotionEvent arg1)
+            {
+            	ed = (EditText) arg0;
+                return false;
+            }
+        });	
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-    
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	// This handler reacts on buttons 0, +2, +5, +10
+	public void HandleClickAddButton(View arg0) {
+		Button btn = (Button) arg0;
+
+		
+		
+// if either Repetion or Weight is selected (double clicked) -> use repetition
+		if (ed == null) {
+			Toast.makeText(this, "no field set, using Repetitions", 2).show();
+			ed = (EditText) findViewById(R.id.editTextRepetitions);
+		}
+		
+		
+		
+		if (btn.getTag().toString().equals("0")) {
+			ed.setText("0");
+			Toast.makeText(this, "set to zero", 2).show();
+			
+		} else {
+			
+//			if there the EditText is empty or not integer, then set to zero 
+			try {
+				tempInt=Integer.parseInt(ed.getText().toString());
+			} catch (Exception ex) {
+				tempInt =0;
+			}
+						
+//			add the Tag of the button to the current value of the TextEdit
+			tempInt=tempInt + Integer.parseInt(btn.getTag().toString());
+			ed.setText(Integer.toString(tempInt)); 
+		}		
+	}
+
+	// This handler reacts on buttons Weight and Repetition
+	
+	public void HandleClickRepWeight(View arg0) {
+//		ed = (EditText) arg0;
+//		Toast.makeText(this, "edit field set", 2).show();
+		
+		
+//		ed.getBackground().setColorFilter(Color.parseColor("#00ff00"), PorterDuff.Mode.DARKEN);
+	}
+
 }
